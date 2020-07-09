3fa48768c93844795ca4ba7af13c073403f98ff8
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-09 16:07:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-reverses = ["Hello there, and how are you?", "Hi again, just making sure it's reversed!"]
+reverses = ["Hello there, and how are you?"]
  
 def reverse_each_word(reserves)
  reverses.collect do |sentence1|
